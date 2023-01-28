31f97523d463365ac3ca669c7fd5f7c1666e8a26
ssidartaa <odssy351@gmail.com>
2023-01-28 13:47:16 -0500

    feat: transactions tests

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -5,10 +5,6 @@ from datetime import datetime
 
 
 class TransactionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Transaction
-        fields = "__all__"
-
     date = serializers.CharField(max_length=8)
     hour = serializers.CharField(max_length=6)
     value = serializers.CharField(max_length=10)
@@ -40,3 +36,31 @@ class TransactionSerializer(serializers.ModelSerializer):
         validated_data["hour"] = hour
 
         return Transaction.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        for key, value in validated_data.items():
+            if key == "value":
+                value = int(value) / 100.00
+
+            if key == "type":
+                for transaction_key, transaction_value in TRANSACTION_TYPE.items():
+                    if transaction_key == value:
+                        value = transaction_value
+
+            if key == "date":
+                formatted_date = f"{value[6:]}/{value[4:6]}/{value[:4]}"
+                value = datetime.strptime(formatted_date, "%d/%m/%Y").date()
+
+            if key == "hour":
+                formatted_time = f"{value[:2]}:{value[2:4]}:{value[4:]}"
+                value = datetime.strptime(formatted_time, "%H:%M:%S").time()
+
+            setattr(instance, key, value)
+
+        instance.save()
+
+        return instance
+
+    class Meta:
+        model = Transaction
+        fields = "__all__"
